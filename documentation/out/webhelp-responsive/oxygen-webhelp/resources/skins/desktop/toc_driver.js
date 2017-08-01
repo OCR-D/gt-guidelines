15fc4a0fa5e0f6837f169eed9c41a6c48fc4209c
tboenig <matthiasboenig@gmx.net>
2017-08-01 13:06:31 +0200

    korr
    Tabelle

--- documentation/out/webhelp-responsive/oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ documentation/out/webhelp-responsive/oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20170731074653", "oxygen-webhelp/search/index-1.js?uniqueId=20170731074653", "oxygen-webhelp/search/index-2.js?uniqueId=20170731074653", "oxygen-webhelp/search/index-3.js?uniqueId=20170731074653"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20170801010009", "oxygen-webhelp/search/index-1.js?uniqueId=20170801010009", "oxygen-webhelp/search/index-2.js?uniqueId=20170801010009", "oxygen-webhelp/search/index-3.js?uniqueId=20170801010009"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
