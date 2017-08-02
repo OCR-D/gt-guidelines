cc9b854ec8c478a7592f8930712982d733bfba36
tboenig <matthiasboenig@gmx.net>
2017-08-02 19:46:35 +0200

    korr

--- documentation/out/webhelp-responsive/oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ documentation/out/webhelp-responsive/oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20170802124921", "oxygen-webhelp/search/index-1.js?uniqueId=20170802124921", "oxygen-webhelp/search/index-2.js?uniqueId=20170802124921", "oxygen-webhelp/search/index-3.js?uniqueId=20170802124921"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20170802073027", "oxygen-webhelp/search/index-1.js?uniqueId=20170802073027", "oxygen-webhelp/search/index-2.js?uniqueId=20170802073027", "oxygen-webhelp/search/index-3.js?uniqueId=20170802073027"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
