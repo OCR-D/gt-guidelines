150d8a88183d52f88a66a64a91049128048a398a
tboenig <matthiasboenig@gmx.net>
2017-08-03 20:27:37 +0200

    korr
    Korrektur

--- documentation/out/webhelp-responsive/oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ documentation/out/webhelp-responsive/oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20170802085523", "oxygen-webhelp/search/index-1.js?uniqueId=20170802085523", "oxygen-webhelp/search/index-2.js?uniqueId=20170802085523", "oxygen-webhelp/search/index-3.js?uniqueId=20170802085523"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20170803082059", "oxygen-webhelp/search/index-1.js?uniqueId=20170803082059", "oxygen-webhelp/search/index-2.js?uniqueId=20170803082059", "oxygen-webhelp/search/index-3.js?uniqueId=20170803082059"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
