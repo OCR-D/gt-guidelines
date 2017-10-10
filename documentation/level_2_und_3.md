ffc4898fbea902b034f9bc9efa1085e444e2cba0
tboenig <matthiasboenig@gmx.net>
2017-10-10 20:17:59 +0200

    Korrektur

---
+++ documentation/level_2_und_3.md
@@ -0,0 +1,3 @@
+# Level 2 und 3
+
+Text
--- documentation/out/webhelp-responsive/oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ documentation/out/webhelp-responsive/oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20171005050251", "oxygen-webhelp/search/index-1.js?uniqueId=20171005050251", "oxygen-webhelp/search/index-2.js?uniqueId=20171005050251", "oxygen-webhelp/search/index-3.js?uniqueId=20171005050251"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20171010080851", "oxygen-webhelp/search/index-1.js?uniqueId=20171010080851", "oxygen-webhelp/search/index-2.js?uniqueId=20171010080851", "oxygen-webhelp/search/index-3.js?uniqueId=20171010080851"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
