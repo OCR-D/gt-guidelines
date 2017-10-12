9610cb37c4e6b59b2d35910ff154a0dd55ea744d
tboenig <matthiasboenig@gmx.net>
2017-10-12 16:55:10 +0200

    Korrektur

--- documentation/out/webhelp-responsive/oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ documentation/out/webhelp-responsive/oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20171010082844", "oxygen-webhelp/search/index-1.js?uniqueId=20171010082844", "oxygen-webhelp/search/index-2.js?uniqueId=20171010082844", "oxygen-webhelp/search/index-3.js?uniqueId=20171010082844"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20171011124431", "oxygen-webhelp/search/index-1.js?uniqueId=20171011124431", "oxygen-webhelp/search/index-2.js?uniqueId=20171011124431", "oxygen-webhelp/search/index-3.js?uniqueId=20171011124431"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
