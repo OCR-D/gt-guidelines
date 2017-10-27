b46aaad80c00947d71f71d3499b0b38ca9ae576c
Boenig <boenig@bbaw.de>
2017-10-27 18:08:53 +0200

    Korrektur

--- documentation/out/webhelp-responsive/oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ documentation/out/webhelp-responsive/oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20171023012716", "oxygen-webhelp/search/index-1.js?uniqueId=20171023012716", "oxygen-webhelp/search/index-2.js?uniqueId=20171023012716", "oxygen-webhelp/search/index-3.js?uniqueId=20171023012716"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20171027055108", "oxygen-webhelp/search/index-1.js?uniqueId=20171027055108", "oxygen-webhelp/search/index-2.js?uniqueId=20171027055108", "oxygen-webhelp/search/index-3.js?uniqueId=20171027055108"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
